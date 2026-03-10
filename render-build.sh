051040bcee20c53c566678eb7a1ae74774a63a9d
mosadajeromed-tech <mosada.jerome.d@gmail.com>
2026-03-10 23:10:02 +0800

    update

--- render-build.sh
+++ render-build.sh
@@ -3,13 +3,13 @@ set -e
 
 echo "=== Building client ==="
 cd client
-npm install
+npm install --include=dev
 npm run build
 cd ..
 
 echo "=== Building server ==="
 cd server
-npm install
+npm install --include=dev
 npx prisma generate
 npx prisma db push
 npm run build
